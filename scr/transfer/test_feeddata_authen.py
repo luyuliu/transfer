ad9fe5b695a2e4eed923fa59c52395eac1495eba
unknown <liuluyu0378@gmail.com>
2018-03-22 16:34:19 -0400

    finish risk

--- scr/transfer/test_feeddata_authen.py
+++ scr/transfer/test_feeddata_authen.py
@@ -17,7 +17,7 @@ def daterange(start_date, end_date):
     for n in range(int ((end_date - start_date).days)):
         yield start_date + timedelta(n)
         
-start_date = date(2017, 1, 29)
+start_date = date(2018, 1, 29)
 end_date = date(2018, 2, 25)
 
 
@@ -38,10 +38,11 @@ for single_date in daterange(start_date, end_date):# enumerate every day in the 
     print(today_date)
     progress_count=0
     total_count=0
-    for each_transfer in db_schedule:
+    for each_trip in db_schedule:
         total_count+=1
-        b_trip_id=each_transfer["trip_id"]
+        b_trip_id=str(each_trip["trip_id"])
         db_real_b_trip=list(db_tripupdate.find({"start_date":today_date,"trip_id":b_trip_id}))
+        #print(today_date,b_trip_id,progress_count)
         if db_real_b_trip==[]:
             progress_count=progress_count+1
             #print(int(today_date),int(b_trip_id))
