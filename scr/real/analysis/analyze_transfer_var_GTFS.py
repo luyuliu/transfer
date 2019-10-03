84bd2e687842eefe3394cea49b41a4f306a316ce
Luyu Liu <liuluyu0378@gmail.com>
2019-10-03 18:25:28 -0400

    Update GTFS version re-cartography work

--- scr/real/analysis/analyze_transfer_var_GTFS.py
+++ scr/real/analysis/analyze_transfer_var_GTFS.py
@@ -66,72 +66,79 @@ def analyze_transfer(start_date, end_date):
     total_TTP_var = 0
     total_risk_var = 0
 
+    # for single_date in date_range:
+    #     if (single_date - date(2018, 3, 10)).total_seconds() <= 0 or (single_date - date(2018, 11, 3)).total_seconds() > 0:
+    #         summer_time = 0
+    #     else:
+    #         summer_time = 1
+    #     today_date = single_date.strftime("%Y%m%d")  # date
+    #     today_seconds = time.mktime(time.strptime(today_date, "%Y%m%d"))
+    #     that_time_stamp = find_gtfs_time_stamp(single_date)
+
+    #     db_today_collection = db_history[today_date]
+    #     db_stops = db_GTFS[str(that_time_stamp) + "_stops"]
+
+    #     db_result = list(db_today_collection.find({}))
+
+    #     # print(db_result)
+
+    #     for single_result in db_result:
+    #         a_stop_id = single_result['a_st']
+    #         try:
+    #             dic_stops[a_stop_id]
+    #         except:
+    #             line = {}
+    #             a_stop = list(db_stops.find({"stop_id": a_stop_id}))
+    #             if a_stop == []:
+    #                 continue
+    #             else:
+    #                 a_stop = a_stop[0]
+    #             line['lat'] = a_stop['stop_lat']
+    #             line['lon'] = a_stop['stop_lon']
+    #             line["totl_TTP"] = 0
+    #             line['totl_c'] = 0
+    #             line['zero_c'] = 0
+    #             line['one_c'] = 0
+    #             line['two_c'] = 0
+    #             line['miss_c'] = 0
+    #             line['crit_c'] = 0
+    #             line['max_TTP'] = 0
+    #             line["totl_var"] = 0
+    #             dic_stops[a_stop_id] = line
+
+    #         switch_status(single_result['status'], dic_stops[a_stop_id])
+    #         if single_result['status'] < 3:
+    #             single_TTP = single_result['b_a_t'] - \
+    #                 single_result['b_t'] + 3600 * summer_time
+
+    #             total_transfer = total_transfer+1
+    #             total_TTP = total_TTP + single_TTP
+    #             if single_result['status'] == 1:
+    #                 total_missed_transfer = total_missed_transfer+1
+
+    #             dic_stops[a_stop_id]["totl_TTP"] += single_TTP
+    #             if single_TTP > dic_stops[a_stop_id]["max_TTP"]:
+    #                 dic_stops[a_stop_id]["max_TTP"] = single_TTP
+
+        
+    #     if total_transfer>0:
+    #         print(today_date, len(dic_stops), total_transfer, round(total_TTP/total_transfer,2), round(total_missed_transfer/total_transfer,4))
+    #     else:
+    #         print(today_date, 0)
+
+    # print("Final average: ", len(dic_stops), total_transfer, round(total_TTP/total_transfer,2), round(total_missed_transfer/total_transfer,4))
+    # average_TTP = total_TTP/total_transfer
+    # average_risk = total_missed_transfer/total_transfer
+    average_TTP = 224.36
+    average_risk = 0.0714
+
+    total_transfer = 0
+    date_range = daterange(start_date, end_date)
     for single_date in date_range:
         if (single_date - date(2018, 3, 10)).total_seconds() <= 0 or (single_date - date(2018, 11, 3)).total_seconds() > 0:
             summer_time = 0
         else:
             summer_time = 1
-        today_date = single_date.strftime("%Y%m%d")  # date
-        today_seconds = time.mktime(time.strptime(today_date, "%Y%m%d"))
-        that_time_stamp = find_gtfs_time_stamp(single_date)
-
-        db_today_collection = db_history[today_date]
-        db_stops = db_GTFS[str(that_time_stamp) + "_stops"]
-
-        db_result = list(db_today_collection.find({}))
-
-        # print(db_result)
-
-        for single_result in db_result:
-            a_stop_id = single_result['a_st']
-            try:
-                dic_stops[a_stop_id]
-            except:
-                line = {}
-                a_stop = list(db_stops.find({"stop_id": a_stop_id}))
-                if a_stop == []:
-                    continue
-                else:
-                    a_stop = a_stop[0]
-                line['lat'] = a_stop['stop_lat']
-                line['lon'] = a_stop['stop_lon']
-                line["totl_TTP"] = 0
-                line['totl_c'] = 0
-                line['zero_c'] = 0
-                line['one_c'] = 0
-                line['two_c'] = 0
-                line['miss_c'] = 0
-                line['crit_c'] = 0
-                line['max_TTP'] = 0
-                line["totl_var"] = 0
-                dic_stops[a_stop_id] = line
-
-            switch_status(single_result['status'], dic_stops[a_stop_id])
-            if single_result['status'] < 3:
-                single_TTP = single_result['b_a_t'] - \
-                    single_result['b_t'] + 3600 * summer_time
-
-                total_transfer = total_transfer+1
-                total_TTP = total_TTP + single_TTP
-                if single_result['status'] == 1:
-                    total_missed_transfer = total_missed_transfer+1
-
-                dic_stops[a_stop_id]["totl_TTP"] += single_TTP
-                if single_TTP > dic_stops[a_stop_id]["max_TTP"]:
-                    dic_stops[a_stop_id]["max_TTP"] = single_TTP
-
-        
-        if total_transfer>0:
-            print(today_date, len(dic_stops), total_transfer, round(total_TTP/total_transfer,2), round(total_missed_transfer/total_transfer,4))
-        else:
-            print(today_date, 0)
-
-    print("Final average: ", len(dic_stops), total_transfer, round(total_TTP/total_transfer,2), round(total_missed_transfer/total_transfer,4))
-    average_TTP = total_TTP/total_transfer
-    average_risk = total_missed_transfer/total_transfer
-    total_transfer = 0
-    date_range = daterange(start_date, end_date)
-    for single_date in date_range:
         today_date = single_date.strftime("%Y%m%d")  # date
         today_seconds = time.mktime(time.strptime(today_date, "%Y%m%d"))
         that_time_stamp = find_gtfs_time_stamp(single_date)
