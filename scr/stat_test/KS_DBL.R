e9754a3babf4e4e54b521ecadb7a05be9945b780
Luyu Liu <liuluyu0378@gmail.com>
2020-01-18 20:06:36 -0500

    Update football data

--- scr/stat_test/KS_DBL.R
+++ scr/stat_test/KS_DBL.R
@@ -1,11 +1,13 @@
-data = read.csv("I:\\OSU\\academic\\transfer_risk\\Transfer\\doc\\nor_dbl.csv")
+football_data = read.csv("I:\\OSU\\academic\\transfer_risk\\Transfer\\doc\\football_test.csv")
 
 
 library(lattice)
 
-ks.test(data$ave_ATTP_normal, data$ave_ATTP_dbl)
+ks.test(football_data$ATTP_football_gtfs, football_data$ATTP_all_gtfs)
+ks.test(football_data$ATTP_football_apc, football_data$ATTP_all_apc)
 
-ks.test(data$ave_tr_normal, data$ave_tr_dbl)
+ks.test(football_data$TR_football_gtfs, football_data$TR_all_gtfs)
+ks.test(football_data$TR_football_apc, football_data$TR_all_apc)
 
-# wilcox.test()
+#wilcox.test()
 
