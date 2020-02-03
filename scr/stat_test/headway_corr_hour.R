485dcb3b473b6dd0ad416b4d072b5cec9c60a235
Luyu Liu <liuluyu0378@gmail.com>
2020-02-03 17:41:26 -0500

    Upload tif (still wrong tho)

--- scr/stat_test/headway_corr_hour.R
+++ scr/stat_test/headway_corr_hour.R
@@ -13,14 +13,14 @@ ggscatter(data, x = "Frequency", y = "ATTP_gtfs",
           cor.coef = TRUE, cor.method = "pearson",
           xlab = "Frequency (hour¯¹)", ylab = "ATTP for original GTFS (minutes)")
 
-attp_apc = ggscatter(data, x = "Frequency", y = "ATTP_apc", size = 4,
-          add = "reg.line", conf.int = TRUE, cor.coef.size = 12, font.x = 30, font.y = 30, x.text = 30, font.tickslab = c(25,"plain"),
+attp_apc = ggscatter(data, x = "Frequency", y = "ATTP_apc", size = 2.5,
+          add = "reg.line", conf.int = TRUE, cor.coef.size = 5, font.x = 12, font.y = 12, x.text = 15, font.tickslab = c(12),
           cor.coef = TRUE, cor.method = "pearson",
           xlab = "Frequency (hour¯¹)", ylab = "ATTP for APC-GTFS (minutes)")
 
 ggexport(attp_apc, plotlist = NULL, filename = 'C:/Users/liu.6544/Documents/GitHub/transfer/doc/corr_pics/attp_apc.png', ncol = NULL,
          nrow = NULL, width = 3200, height = 3200, pointsize = 12,
-         res = 300, verbose = TRUE)
+         res = 800, verbose = TRUE)
 
 
 ggscatter(data, x = "Frequency", y = "TR_gtfs", 
@@ -28,13 +28,13 @@ ggscatter(data, x = "Frequency", y = "TR_gtfs",
           cor.coef = TRUE, cor.method = "pearson",
           xlab = "Frequency (hour¯¹)", ylab = "TR for original GTFS (%)")
 
-tr_apc = ggscatter(data, x = "Frequency", y = "TR_apc", size = 4,
-          add = "reg.line", conf.int = TRUE, cor.coef.size = 12, font.x = 30, font.y = 30, x.text.font = 30, font.tickslab = c(25,"plain"),
-          cor.coef = TRUE, cor.method = "pearson",
+tr_apc = ggscatter(data, x = "Frequency", y = "TR_apc", size = 2.5,
+                   add = "reg.line", conf.int = TRUE, cor.coef.size = 5, font.x = 12, font.y = 12, x.text = 15, font.tickslab = c(12),
+                   cor.coef = TRUE, cor.method = "pearson",
           xlab = "Frequency (hour¯¹)", ylab = "TR for APC-GTFS (%)")
 
 ggexport(tr_apc, plotlist = NULL, filename = 'C:/Users/liu.6544/Documents/GitHub/transfer/doc/corr_pics/tr_apc.png', ncol = NULL,
          nrow = NULL, width = 3200, height = 3200, pointsize = 12,
-         res = 300, verbose = TRUE)
+         res = 800, verbose = TRUE)
 
 
